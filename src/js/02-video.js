96bf9388dc2cdaafd8529b860bf7432766d0e7fa
Taras Kozmovych <taraskom0404@gmail.com>
2022-08-07 02:39:26 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const video = document.querySelector('iframe');
+const player = new Player(video);
+player.on('timeupdate', throttle(videoTime, 1000));
+function videoTime(time) {
+    localStorage.setItem(
+        'videoplayer-current-time',
+        JSON.stringify(time.seconds)
+    );
+}
+const saveVideoTime = localStorage.getItem('videoplayer-current-time');
+if (saveVideoTime) player.setCurrentTime(saveVideoTime);
