a6f25015223231419d24e446d2e347cebb148a8d
Taras Kozmovych <taraskom0404@gmail.com>
2022-08-07 01:26:43 +0300

    task-1-finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const ulGalleryRef = document.querySelector(".gallery");
+
+galleryItems.forEach(({ preview, original, description }) => {
+    const addGalleryItems = `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    ulGalleryRef.innerHTML += addGalleryItems;
+});
+const lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: "250",
+});
+
 
 console.log(galleryItems);
